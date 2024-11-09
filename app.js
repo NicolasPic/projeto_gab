af454db39b857dfc902b690e95e6634644f74324
Nicolas Piccoli <nicolaslas-piccoli@hotmail.com>
2024-11-09 09:09:22 -0300

    Base do projeto concluido 1.0.1

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const homepage = require("./routes/homepage")
+const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
 
 //Configurações
+    //Template Engine
+        //handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('veiw engine', 'handlebars')
+        //bodyparse
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
 
     //rotas
         app.use('/home',homepage)
@@ -11,5 +20,5 @@ const homepage = require("./routes/homepage")
 
 //outros
 app.listen(8081, function(){
-    console.log("Servidor rodando na url http://localhost:8081");
+    console.log('Servidor rodando na url http://localhost:8081');
 });
