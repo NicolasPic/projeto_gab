8b19a18a8712d9b214fcf16eadb41600642d3950
Nicolas Piccoli <nicolaslas-piccoli@hotmail.com>
2024-11-10 09:55:17 -0300

    Foi adicionando a logica basica do handlebars 1.0.2

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
-const homepage = require("./routes/homepage")
-const handlebars = require("express-handlebars")
-const bodyParser = require("body-parser")
+const homepage = require("./routes/homePage")
+const { engine } = require('express-handlebars');
 
 //Configurações
     //Template Engine
         //handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('veiw engine', 'handlebars')
+        app.engine('handlebars', engine({ defaultLayout: 'main' }));
+        app.set('view engine', 'handlebars');
         //bodyparse
-        app.use(bodyParser.urlencoded({extended: false}))
-        app.use(bodyParser.json())
+        app.use(express.urlencoded({ extended: false }));
+        app.use(express.json());
 
     //rotas
         app.use('/home',homepage)
--- routes/homepage.js
+++ routes/homepage.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('/',(req, res) => {
-    res.send("Pagina principal")
-})
+router.get('/', (req, res) => {
+    res.render('pages/home', { 
+        title: 'Página Inicial',
+        customHeaderHome: true  // Ativa o cabeçalho principal
+    });
+});
 
 
 module.exports = router
