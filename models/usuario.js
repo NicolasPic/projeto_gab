0d11c5fa4d1561f5c8d363393575091f6317efa9
Nicolas Piccoli <nicolaslas-piccoli@hotmail.com>
2024-11-26 22:15:27 -0300

    foi adicionado o login

--- models/usuario.js
+++ models/usuario.js
@@ -1,1 +1,43 @@
-const sequelize = require('../config/DB/database');
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/DB/database');
+
+// Definindo o modelo de Usuario
+const Usuario = sequelize.define('Usuario', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    unique: true, 
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,  
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,  
+    unique: true,      
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,  
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,  
+    unique: true,      
+    validate: {
+      isEmail: true,  
+    },
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+}, {
+  tableName: 'usuarios', 
+  timestamps: false,
+});
+
+module.exports = Usuario;
