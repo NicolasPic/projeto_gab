a8340c72e63f3f4f17b26d5f05805bd3fe7d873e
NicolasPic <nicolaslas-piccoli@hotmail.com>
2024-12-06 15:42:46 -0300

    Ajustes no socket.io

--- config/socket/gameSocket.js
+++ config/socket/gameSocket.js
@@ -1,24 +1,30 @@
 const salas = {};  // Armazena as salas ativas
 const usuarios = {};
+
 function configurarSocket(io) {
     io.on('connection', (socket) => {
         console.log('Novo jogador conectado:', socket.id);
 
-        socket.on('sala', ({codigoSala,usuarioID}) => {
-            // Verifica se o jogador já está na sala
-
+        socket.on('sala', ({codigoSala, usuarioID}) => {
             usuarios[socket.id] = usuarioID;
-            if (salas[codigoSala] && salas[codigoSala].includes(socket.id)) {
-                console.log(`Jogador ${socket.id} já está na sala ${codigoSala}.`);
 
-            } else if (salas[codigoSala]) {
-                // A sala existe, mas o jogador não está nela
-                console.log(`Jogador ${socket.id} se conectando à sala ${codigoSala}.`);
-                socket.join(codigoSala);
-                salas[codigoSala].push(socket.id);
+            // Verifica se a sala já existe
+            if (salas[codigoSala]) {
+                // Verifica se o jogador já está na sala
+                if (salas[codigoSala].includes(socket.id)) {
+                    console.log(`Jogador ${socket.id} já está na sala ${codigoSala}.`);
+                } else {
+                    // O jogador não está na sala, então ele entra nela
+                    console.log(`Jogador ${socket.id} se conectando à sala ${codigoSala}.`);
+                    socket.join(codigoSala);
+                    salas[codigoSala].push(socket.id);
 
-                socket.handshake.session.sala = codigoSala;
-                socket.handshake.session.save();
+                    // Salva os dados na sessão
+                    socket.handshake.session.sala = codigoSala;
+                    socket.handshake.session.usuarioID = usuarioID;
+                    socket.handshake.session.save();
+
+                }
             } else {
                 // A sala não existe, cria uma nova
                 console.log(`Criando sala com id: ${codigoSala}.`);
@@ -28,17 +34,16 @@ function configurarSocket(io) {
 
                 // Atualiza a sessão com o código da sala
                 socket.handshake.session.sala = codigoSala;
+                socket.handshake.session.usuarioID = usuarioID;
                 socket.handshake.session.save();
+
                 console.log(`Sala ${codigoSala} criada e jogador ${socket.id} entrou.`);
             }
-
         });
 
         socket.on('disconnect', function(){
-            console.log('user ' + usuarios[socket.id] + ' disconnected');
-            // remove saved socket from users object
-            delete usuarios[socket.id];
-          });
+            console.log('usuario ' + socket.id + 'disconectado');
+        });
 
     });
 }
